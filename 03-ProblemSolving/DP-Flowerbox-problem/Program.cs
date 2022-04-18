66ec5b5006ac1cf111412bc5c2d716f7cc297d95
Muhammad Adil <muhammad.adil@un.org>
2022-04-19 02:41:19 +0500

    added flowerbox problem dp

---
+++ 03-ProblemSolving/DP-Flowerbox-problem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DP_Flowerbox_problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(flowerbox(new List<int> { 3, 10, 3,1,2}));
+            Console.WriteLine(flowerbox(new List<int> { 9,10,9}));
+        }
+
+        public static int flowerbox(List<int> spots)
+        {
+            int a = 0;
+            int b = 0; // intial spots set to 0
+
+            foreach (int value in spots)
+            {
+                int olda = a;
+                int oldb = b;
+
+                a = oldb;
+                b= Math.Max(olda + value, oldb);
+
+            }
+
+            return b;
+
+        }
+    }
+}
---
+++ 03-ProblemSolving/DP_flowerbox_problem/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine(flowerbox(new List<int> { 3, 10, 3, 1, 2 }));
+    }
+
+    public static int flowerbox(List<int> list)
+    {
+        int a = 0;
+        int b = 0;
+
+        foreach (int val in list)
+        {
+            a = b;
+            b = Math.Max(a + val, b);
+        }
+        return b;
+    }
+
+}
